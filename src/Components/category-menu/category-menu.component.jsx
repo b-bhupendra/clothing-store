a20635874d34fd598a2c5c677666e57d4ea308a8
Bhupendra Singh Rawat <bhupendrasr08@gmail.com>
2022-12-11 21:14:45 +0530

    updating our directory page with directory component and category component

---
+++ src/Components/category-menu/category-menu.component.jsx
@@ -0,0 +1,28 @@
+
+import CategoryItem from "../category-item/category-item.component"
+import "../category-menu/category-menu.styles.scss"
+const CategoryMenu = ({ categories }) => {
+
+   
+      
+return (
+        
+    <div className="categories-container">
+                  {
+                    categories.map((category) => {
+                            return (
+                            <CategoryItem key = {category.id} category = {category} />
+                        )
+                      
+                      })
+                  
+                  }   
+                
+                 
+          </div>
+          
+          )
+};
+
+
+export default CategoryMenu;
